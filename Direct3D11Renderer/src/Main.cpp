5ea97e24029c927daa338b2c80da508b9f9df4bf
bberisford <brandonberisford@gmail.com>
2025-05-07 22:17:54 -0500

    Adding try catch

--- Direct3D11Renderer/src/Main.cpp
+++ Direct3D11Renderer/src/Main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <string>
 #include "Window.h"
+#include "D3Exception.h"
 
 int WINAPI wWinMain(
     _In_ HINSTANCE hInstance,
@@ -8,20 +9,36 @@ int WINAPI wWinMain(
     _In_ LPWSTR lpCmdLine,
     _In_ int nCmdShow)
 {
-	Window wnd(800, 600, L"D3DEngine");
-
-    MSG msg;
-    BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-	{
-        TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-    if (gResult == -1)
+    try
     {
-        return -1;
-    }
+        Window wnd(800, 600, L"D3DEngine");
 
-    return static_cast<int>(msg.wParam);
+        MSG msg;
+        BOOL gResult;
+        while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if (gResult == -1)
+        {
+            return -1;
+        }
+
+        return static_cast<int>(msg.wParam);
+    }
+    catch (const D3Exception& e)
+    {
+		MessageBox(nullptr, (LPCWSTR)e.what(), (LPCWSTR)e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+    }
+    catch (const std::exception& e)
+    {
+		MessageBox(nullptr, (LPCWSTR)e.what(), L"Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, L"Unknown Exception", L"Error", MB_OK | MB_ICONEXCLAMATION);
+    }
+	
 }
